ddb9090dee948d873a9c981adc160525fa58cfe5
Kamicloud <cy602939074@qq.com>
2022-07-28 20:46:35 +0800

    put output path to config file

--- liberator-generators/src/main/java/com/github/kamicloud/liberator/generators/StubGenerator.java
+++ liberator-generators/src/main/java/com/github/kamicloud/liberator/generators/StubGenerator.java
@@ -15,28 +15,29 @@ public class StubGenerator extends BaseGenerator {
     public void run() {
         this.generatorProperties.getStubs().forEach(stub -> {
             GeneratorProperties.StubName stubName = stub.getStub();
-            String templatePath = stub.getPath();
+            String templatePath = stub.getTemplate();
+            String outputPath = stub.getPath();
 
             if (stubName == GeneratorProperties.StubName.OUTPUT) {
-                generateAll(templatePath, output);
+                generateTo(templatePath, outputPath, output);
             }
 
             if (stubName == GeneratorProperties.StubName.ERROR) {
                 output.getErrors().forEach(errorStub -> {
-                    generateAll(templatePath, errorStub);
+                    generateTo(templatePath, outputPath, errorStub);
                 });
             }
 
             if (stubName == GeneratorProperties.StubName.TEMPLATE) {
                 output.getTemplates().forEach((templateName, templateStub) -> {
-                    generateAll(templatePath, templateStub);
+                    generateTo(templatePath, outputPath, templateStub);
                 });
             }
 
             if (stubName == GeneratorProperties.StubName.ENUM) {
                 output.getTemplates().forEach((templateName, template) -> {
                     template.getEnums().forEach(enumStub -> {
-                        generateAll(templatePath, enumStub);
+                        generateTo(templatePath, outputPath, enumStub);
                     });
                 });
             }
@@ -44,7 +45,7 @@ public class StubGenerator extends BaseGenerator {
             if (stubName == GeneratorProperties.StubName.MODEL) {
                 output.getTemplates().forEach((templateName, template) -> {
                     template.getModels().forEach(modelStub -> {
-                        generateAll(templatePath, modelStub);
+                        generateTo(templatePath, outputPath, modelStub);
                     });
                 });
             }
@@ -52,7 +53,7 @@ public class StubGenerator extends BaseGenerator {
             if (stubName == GeneratorProperties.StubName.CONTROLLER) {
                 output.getTemplates().forEach((templateName, template) -> {
                     template.getControllers().forEach(controllerStub -> {
-                        generateAll(templatePath, controllerStub);
+                        generateTo(templatePath, outputPath, controllerStub);
                     });
                 });
             }
@@ -61,48 +62,42 @@ public class StubGenerator extends BaseGenerator {
                 output.getTemplates().forEach((templateName, template) -> {
                     template.getControllers().forEach(controllerStub -> {
                         controllerStub.getActions().forEach(actionStub -> {
-                            generateAll(templatePath, actionStub);
+                            generateTo(templatePath, outputPath, actionStub);
                         });
                     });
                 });
             }
 
             if (stubName == GeneratorProperties.StubName.CURRENT_TEMPLATE) {
-                generateAll(templatePath, output.getCurrentTemplate());
+                generateTo(templatePath, outputPath, output.getCurrentTemplate());
             }
 
             if (stubName == GeneratorProperties.StubName.CURRENT_TEMPLATE_ENUM) {
                 output.getCurrentTemplate().getEnums().forEach(enumStub -> {
-                    generateAll(templatePath, enumStub);
+                    generateTo(templatePath, outputPath, enumStub);
                 });
             }
 
             if (stubName == GeneratorProperties.StubName.CURRENT_TEMPLATE_MODEL) {
                 output.getCurrentTemplate().getModels().forEach(modelStub -> {
-                    generateAll(templatePath, modelStub);
+                    generateTo(templatePath, outputPath, modelStub);
                 });
             }
 
             if (stubName == GeneratorProperties.StubName.CURRENT_TEMPLATE_CONTROLLER) {
                 output.getCurrentTemplate().getControllers().forEach(controllerStub -> {
-                    generateAll(templatePath, controllerStub);
+                    generateTo(templatePath, outputPath, controllerStub);
                 });
             }
 
             if (stubName == GeneratorProperties.StubName.CURRENT_TEMPLATE_ACTION) {
                 output.getCurrentTemplate().getControllers().forEach(controllerStub -> {
                     controllerStub.getActions().forEach(actionStub -> {
-                        generateAll(templatePath, actionStub);
+                        generateTo(templatePath, outputPath, actionStub);
                     });
                 });
             }
         });
-    }
-
-    public void generateAll(String templatePath, Object param) {
-        String outputPath = this.stringUtil.renderTemplate(templatePath, param);
-
-        this.generateTo(templatePath, outputPath, param);
     }
 
     public void generateTo(String stubPath, String outputPath, Object param) {
