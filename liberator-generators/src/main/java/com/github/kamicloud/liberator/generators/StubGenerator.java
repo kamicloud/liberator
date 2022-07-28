80c25fe1da98073377c6d959e5583559b5d89e81
Kamicloud <cy602939074@qq.com>
2022-07-28 08:48:46 +0800

    enhance stub generator

--- liberator-generators/src/main/java/com/github/kamicloud/liberator/generators/StubGenerator.java
+++ liberator-generators/src/main/java/com/github/kamicloud/liberator/generators/StubGenerator.java
@@ -17,12 +17,26 @@ public class StubGenerator extends BaseGenerator {
             GeneratorProperties.StubName stubName = stub.getStub();
             String templatePath = stub.getTemplate();
 
+            if (stubName == GeneratorProperties.StubName.OUTPUT) {
+                generateAll(templatePath, output);
+            }
+
+            if (stubName == GeneratorProperties.StubName.ERROR) {
+                output.getErrors().forEach(errorStub -> {
+                    generateAll(templatePath, errorStub);
+                });
+            }
+
+            if (stubName == GeneratorProperties.StubName.TEMPLATE) {
+                output.getTemplates().forEach((templateName, templateStub) -> {
+                    generateAll(templatePath, templateStub);
+                });
+            }
+
             if (stubName == GeneratorProperties.StubName.ENUM) {
                 output.getTemplates().forEach((templateName, template) -> {
                     template.getEnums().forEach(enumStub -> {
-                        String outputPath = this.stringUtil.renderTemplate(templatePath + "_path", enumStub);
-
-                        this.generateTo(templatePath, outputPath, enumStub);
+                        generateAll(templatePath, enumStub);
                     });
                 });
             }
@@ -30,13 +44,65 @@ public class StubGenerator extends BaseGenerator {
             if (stubName == GeneratorProperties.StubName.MODEL) {
                 output.getTemplates().forEach((templateName, template) -> {
                     template.getModels().forEach(modelStub -> {
-                        String outputPath = this.stringUtil.renderTemplate(templatePath + "_path", modelStub);
+                        generateAll(templatePath, modelStub);
+                    });
+                });
+            }
 
-                        this.generateTo(templatePath, outputPath, modelStub);
+            if (stubName == GeneratorProperties.StubName.CONTROLLER) {
+                output.getTemplates().forEach((templateName, template) -> {
+                    template.getControllers().forEach(controllerStub -> {
+                        generateAll(templatePath, controllerStub);
+                    });
+                });
+            }
+
+            if (stubName == GeneratorProperties.StubName.ACTION) {
+                output.getTemplates().forEach((templateName, template) -> {
+                    template.getControllers().forEach(controllerStub -> {
+                        controllerStub.getActions().forEach(actionStub -> {
+                            generateAll(templatePath, actionStub);
+                        });
+                    });
+                });
+            }
+
+            if (stubName == GeneratorProperties.StubName.CURRENT_TEMPLATE) {
+                generateAll(templatePath, output.getCurrentTemplate());
+            }
+
+            if (stubName == GeneratorProperties.StubName.CURRENT_TEMPLATE_ENUM) {
+                output.getCurrentTemplate().getEnums().forEach(enumStub -> {
+                    generateAll(templatePath, enumStub);
+                });
+            }
+
+            if (stubName == GeneratorProperties.StubName.CURRENT_TEMPLATE_MODEL) {
+                output.getCurrentTemplate().getModels().forEach(modelStub -> {
+                    generateAll(templatePath, modelStub);
+                });
+            }
+
+            if (stubName == GeneratorProperties.StubName.CURRENT_TEMPLATE_CONTROLLER) {
+                output.getCurrentTemplate().getControllers().forEach(controllerStub -> {
+                    generateAll(templatePath, controllerStub);
+                });
+            }
+
+            if (stubName == GeneratorProperties.StubName.CURRENT_TEMPLATE_ACTION) {
+                output.getCurrentTemplate().getControllers().forEach(controllerStub -> {
+                    controllerStub.getActions().forEach(actionStub -> {
+                        generateAll(templatePath, actionStub);
                     });
                 });
             }
         });
+    }
+
+    public void generateAll(String templatePath, Object param) {
+        String outputPath = this.stringUtil.renderTemplate(templatePath + "_path", param);
+
+        this.generateTo(templatePath, outputPath, param);
     }
 
     public void generateTo(String stubPath, String outputPath, Object param) {
